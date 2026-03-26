5e8bb7aa95624161fcb47e831d1ab137ba268261
likun sahoo <likunsahoo486@gmail.com>
2026-03-26 14:52:17 +0530

    pull

--- NewFrontend/src/App.jsx
+++ NewFrontend/src/App.jsx
@@ -5,6 +5,7 @@ import Topbar from "./Pages/Topbar/Topbar";
 import Navbar from "./Pages/Navbar/Navbar";
 import Footer from "./Pages/Footer/Footer";
 import Home from "./Pages/Home/Home";
+import AboutUs from "./Pages/AboutUs/AboutUs";
 
 
 
@@ -17,6 +18,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
 
       <Footer />
---
+++ NewFrontend/src/Pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Breadcrumb from '../../Component/Breadcrumb/Breadcrumb'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Breadcrumb />
+    </div>
+  )
+}
+
+export default AboutUs
