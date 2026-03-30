aab4d6292054132244a8fc3b438d52bf08ac2c50
likun sahoo <likunsahoo486@gmail.com>
2026-03-30 15:14:09 +0530

    pull

--- NewFrontend/src/Pages/AppDevlopement/AppDevlopement.jsx
+++ NewFrontend/src/Pages/AppDevlopement/AppDevlopement.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import AppDevelopmentHero from '../../Component/AppDevelopmentHero/AppDevelopmentHero'
 import AppDevlopmentAbout from '../../Component/AppDevlopmentAbout/AppDevlopmentAbout'
 import AppDevlopmentOurService from '../../Component/AppDevlopmentOurService/AppDevlopmentOurService'
+import AppDevlopmentWhyChooseUs from '../../Component/AppDevlopmentWhyChooseUs/AppDevlopmentWhyChooseUs'
+import AppDevlopmentWorkProcess from '../../Component/AppDevlopmentWorkProcess/AppDevlopmentWorkProcess'
+import AppDevlopmentOurPortfolio from '../../Component/AppDevlopmentOurPortfolio/AppDevlopmentOurPortfolio'
 
 const AppDevlopement = () => {
   return (
@@ -9,6 +12,9 @@ const AppDevlopement = () => {
       <AppDevelopmentHero />
       <AppDevlopmentAbout />
       <AppDevlopmentOurService />
+      <AppDevlopmentWhyChooseUs />
+      <AppDevlopmentWorkProcess />
+      <AppDevlopmentOurPortfolio />
     </div>
   )
 }
