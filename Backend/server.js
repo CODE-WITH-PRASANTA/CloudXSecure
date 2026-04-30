3f5faab325467295ba3eb7c41aa900acb6f49fba
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-04-30 16:23:15 +0530

    Done

--- Backend/server.js
+++ Backend/server.js
@@ -20,17 +20,10 @@ const categoryRoutes = require("./routes/categoryRoutes");
 const app = express();
 
 /* ================= CORS ================= */
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
-      "http://localhost:5176",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
